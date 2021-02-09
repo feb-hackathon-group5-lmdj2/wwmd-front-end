530d8d5e3f544cd968f022a179d5954c8c7939d0
SpideyFanDan <dsmith.teach@gmail.com>
2021-02-09 17:52:35 -0500

    added social media buttons - facebook and twitter

--- src/Share.js
+++ src/Share.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { FacebookShareButton, FacebookIcon } from 'react-share';
+import {
+	FacebookShareButton,
+	FacebookIcon,
+	TwitterShareButton,
+	TwitterIcon,
+} from 'react-share';
 
-const Share = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function Share(props) {
+	return (
+		<div>
+			<FacebookShareButton
+				url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
+				quote={'I got the COVID vaccine!'}
+				hashtag='#covid19'
+				className={FacebookShareButton}>
+				<FacebookIcon size={36} />
+			</FacebookShareButton>
+			<TwitterShareButton
+				url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
+				title={'I got the COVID vaccine!'}
+				hashtags='#covid19'
+				className={TwitterShareButton}>
+				<TwitterIcon size={36} />
+			</TwitterShareButton>
+		</div>
+	);
+}
 
 export default Share;
