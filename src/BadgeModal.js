bf77ca0497bb86f1c493d7a87436a160e676bda3
melodeenae5 <danaekirby5@gmail.com>
2021-02-10 13:01:31 -0600

    added badgemodal and test carousel as placeholder

--- src/BadgeModal.js
+++ src/BadgeModal.js
@@ -5,25 +5,43 @@ import {
 	TwitterShareButton,
 	TwitterIcon,
 } from 'react-share';
+import BootstrapModal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import CarouselHome from './CarouselHome';
 
-function BadgeModal(props) {
+function BadgeModal({ show, handleClose }) {
 	return (
-		<div>
-			<FacebookShareButton
-				url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
-				quote={'I got the COVID vaccine!'}
-				hashtag='#covid19'
-				className={FacebookShareButton}>
-				<FacebookIcon size={36} />
-			</FacebookShareButton>
-			<TwitterShareButton
-				url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
-				title={'I got the COVID vaccine!'}
-				hashtags='#covid19'
-				className={TwitterShareButton}>
-				<TwitterIcon size={36} />
-			</TwitterShareButton>
-		</div>
+		<BootstrapModal show={show} onHide={handleClose}>
+			<BootstrapModal.Header closeButton>
+				<BootstrapModal.Title>Modal title</BootstrapModal.Title>
+			</BootstrapModal.Header>
+			<BootstrapModal.Body>
+				Woohoo, you're reading this text in a modal!
+				<CarouselHome />
+			</BootstrapModal.Body>
+			<BootstrapModal.Footer>
+				<Button variant='secondary' onClick={handleClose}>
+					Close
+				</Button>
+			</BootstrapModal.Footer>
+		</BootstrapModal>
+
+		// <div>
+		// 	<FacebookShareButton
+		// 		url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
+		// 		quote={'I got the COVID vaccine!'}
+		// 		hashtag='#covid19'
+		// 		className={FacebookShareButton}>
+		// 		<FacebookIcon size={36} />
+		// 	</FacebookShareButton>
+		// 	<TwitterShareButton
+		// 		url={'https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'}
+		// 		title={'I got the COVID vaccine!'}
+		// 		hashtags='#covid19'
+		// 		className={TwitterShareButton}>
+		// 		<TwitterIcon size={36} />
+		// 	</TwitterShareButton>
+		// </div>
 	);
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card'
-import CarouselHome from './CarouselHome'
-
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import CarouselHome from './CarouselHome';
+import BadgeModal from './BadgeModal';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
-    return (
-			<>
-				<Card>
-					<Card.Title>Grab your Badge 1 Card</Card.Title>
-				</Card>
-				<CarouselHome />
-				<Card>
-					<Card.Title>Grab your Badge 1 Card again</Card.Title>
-                    
-				</Card>
-				<Card>
-					<Card.Title>Link to CDC </Card.Title>
-				</Card>
-			</>
-		)
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<Card>
+			<Card>
+				<Card.Title>Grab your Badge 1 Card</Card.Title>
+				<Button variant='secondary' onClick={handleShow}>
+					Open Modal
+				</Button>
+			</Card>
+			<CarouselHome />
+			<Card>
+				<Card.Title>Grab your Badge 1 Card again</Card.Title>
+				<Button variant='secondary' onClick={handleShow}>
+					Open Modal
+				</Button>
+			</Card>
+			<Card>
+				<Card.Title>Link to CDC </Card.Title>
+			</Card>
+			<BadgeModal show={show} handleClose={handleClose} />
+		</Card>
+	);
 };
 
 export default Home;
