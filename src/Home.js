16a477ccfe20871b47109f1a30d822b69b0968d8
SpideyFanDan <dsmith.teach@gmail.com>
2021-02-10 17:05:17 -0500

    added css

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ import CarouselHome from './CarouselHome';
 import BadgeModal from './BadgeModal';
 
 const Home = () => {
-  	const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
@@ -26,7 +26,7 @@ const Home = () => {
 							We’ve worked hard to stop COVID-19 in it’s tracks, let’s keep up
 							the good work!
 						</p>
-						<Image src='./images/max-header.png' fluid />
+						<img src={require('./images/max-header.png')} fluid />
 					</Jumbotron>
 				</Col>
 			</Row>
@@ -37,24 +37,30 @@ const Home = () => {
 			</Row>
 			<Row>
 				<Col md={{ span: 3, offset: 3 }}>
-					<Card className='badge-card'>
-						<Card.Title>Grab your Badge 1 Card</Card.Title>
-						<Button className='badge-btn' onClick={handleShow}>START NOW</Button>
+					<Card className='text-center' className='badge-card'>
+						<Card.Title className='badge-card-title'>
+							Grab your Badge 1 Card
+						</Card.Title>
+						<Button className='badge-btn' onClick={handleShow}>
+							START NOW
+						</Button>
 					</Card>
 				</Col>
 			</Row>
 			<Row>
 				<Col md={{ span: 3, offset: 3 }}>
 					<Button className='cdc-card'>
+						<hr></hr>
 						<h4 className='cdc-click'>CLICK HERE</h4>{' '}
 						<p className='cdc-p'>
 							To find out more from the CDC about COVID-19 vaccinations and when
 							you can get it in your state
 						</p>
+						<hr></hr>
 					</Button>
 				</Col>
 			</Row>
-    <BadgeModal show={show} handleClose={handleClose} />
+			<BadgeModal show={show} handleClose={handleClose} />
 		</Container>
 	);
 };
