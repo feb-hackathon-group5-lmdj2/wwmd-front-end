c2e878817c69e46dfeeceae072c28b29b5935f19
SpideyFanDan <dsmith.teach@gmail.com>
2021-02-11 12:30:06 -0500

    updated css, images, and fonts

--- src/BadgeModal.js
+++ src/BadgeModal.js
@@ -1,27 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import {
-	FacebookShareButton,
-	FacebookIcon,
-	TwitterShareButton,
-	TwitterIcon,
-} from 'react-share';
+// import {
+// 	FacebookShareButton,
+// 	FacebookIcon,
+// 	TwitterShareButton,
+// 	TwitterIcon,
+// } from 'react-share';
 import BootstrapModal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { Image, Container, Row, Col } from 'react-bootstrap';
 import ModalCarousel from './ModalCarousel';
+import fingerpoint from './images/fingerpoint.png';
 
 function BadgeModal({ show, handleClose }) {
 	return (
 		<BootstrapModal show={show} onHide={handleClose}>
-			<BootstrapModal.Header closeButton className='badge-modal-header'>
-				<BootstrapModal.Title className='modal-title'>
-					Badges
-				</BootstrapModal.Title>
-			</BootstrapModal.Header>
+			<Container className='badge-modal-header'>
+				<BootstrapModal.Header className='badge-modal-header'>
+					<Row>
+						<BootstrapModal.Title className='modal-title'>
+							Grab your badge!
+							<br />
+							<p>
+								Select an image and share with your friends where you're at in
+								your vaccine journey.
+							</p>
+						</BootstrapModal.Title>
+					</Row>
+					<Row>
+						<Image
+							fluid
+							className=''
+							src={fingerpoint}
+							alt='finger point max'
+						/>
+					</Row>
+				</BootstrapModal.Header>
+			</Container>
 			<BootstrapModal.Body className='badge-modal'>
 				<ModalCarousel />
 			</BootstrapModal.Body>
-			<BootstrapModal.Footer className='modal-footer' >
+			<BootstrapModal.Footer className='modal-footer'>
 				<Button
 					variant='secondary'
 					onClick={handleClose}
--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,8 @@ import CarouselHome from './CarouselHome';
 import BadgeModal from './BadgeModal';
 import Axios from 'axios';
 import headerImg from './images/max-header.png';
-
+import hearts from './images/hearts.png';
+import bandaidMask from './images/bandaidmask.png';
 const Home = () => {
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
@@ -22,57 +23,111 @@ const Home = () => {
 	const url = `https://blooming-earth-00957.herokuapp.com/songs/9`;
 
 	return (
-		// <Container fluid='md'>
-		<div className='site-wrapper'>
-			<div className='jumbo-wrapper'>
-				<Jumbotron className='header' class='container'>
-					<h4 className='header-title'>Hi, I'm Max the mask!</h4>
-					<p className='header-p'>
-						We’ve worked hard to stop COVID-19 in it’s tracks, let’s keep up the
-						good work!
-					</p>
-					{/* <Image className='header-image' src={headerImg} /> */}
-				</Jumbotron>
-			</div>
+		<Container fluid='sm'>
+			<Row>
+				<Col xs={3} md={2}></Col>
+				<Col xs={12} md={8}>
+					<Jumbotron fluid className='header' class='container'>
+						<Row>
+							<Col xs={6} md={4}>
+								<h4 className='header-title'>Hi, I'm Max the mask!</h4>
+								<p className='header-p'>
+									We’ve worked hard to stop COVID-19 in it’s tracks, let’s keep
+									up the good work!
+								</p>
+							</Col>
+							<Col xs={6} md={4}>
+								<Image fluid className='header-image' src={headerImg} />
+							</Col>
+						</Row>
+					</Jumbotron>
+				</Col>
+				<Col xs={3} md={2}></Col>
+			</Row>
 			<div className='talk-wrapper'>
-				<Card className='talk-card'>
-					<Card.Title className='cdc-click'>
-						Let’s talk covid vaccines
-					</Card.Title>
-					<Card.Text className='header-p'>
-						How to have productive conversation about COVID-19 Vaccines
-					</Card.Text>
-				</Card>
+				<Row>
+					<Col xs={3} md={2}></Col>
+					<Col xs={12} md={8}>
+						<Card border='light' className='talk-card' className='text-left'>
+							<Row>
+								<Col xs={3} md={2}></Col>
+								<Col xs={15} md={10}>
+									<Card.Title className='cdc-click'>
+										Let’s talk Covid-19 vaccines
+									</Card.Title>
+								</Col>
+								<Col xs={3} md={2}></Col>
+							</Row>
+							<Row>
+								<Col xs={3} md={2}></Col>
+								<Col xs={12} md={8}>
+									<Card.Text className='header-p'>
+										How to have productive conversation about COVID-19 Vaccines
+									</Card.Text>
+								</Col>
+								<Col xs={3} md={2}></Col>
+							</Row>
+						</Card>
+					</Col>
+					<Col xs={3} md={2}></Col>
+				</Row>
 			</div>
 			<div className='card-wrapper'>
-				<CarouselHome />
+				<Row>
+					<Col xs={3} md={2}></Col>
+					<Col xs={12} md={8}>
+						<CarouselHome />
+					</Col>
+					<Col xs={3} md={2}></Col>
+				</Row>
 			</div>
 
-			<div className='badge-wrapper'>
-				<Card className='text-center' >
-					<Card.Title className='cdc-click'>Grab your Badge 1 Card</Card.Title>
-					<Button className='badge-btn' onClick={handleShow}>
-						START NOW
-					</Button>
+			<div>
+				<Card className='badge-wrapper' className='text-center'>
+					<Card.Title className='cdc-click'>Grab your Badge!</Card.Title>
+					<Card.Body>
+						<Row>
+							<p>
+								Share with your friends where you're at in your vaccine journey.
+							</p>
+						</Row>
+						<Row>
+							<Col xs={3} md={2}>
+								<Image fluid className='badge-image' src={hearts} />
+							</Col>
+							<Col xs={18} md={12}>
+								<Button className='badge-btn' onClick={handleShow}>
+									START NOW
+								</Button>
+							</Col>
+							<Col xs={3} md={2}>
+								<Image fluid className='badge-image' src={bandaidMask} />
+							</Col>
+						</Row>
+					</Card.Body>
 				</Card>
 			</div>
-			<Row className='cdc-wrapper'>
-				<a
-					target='_blank'
-					href='https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'>
-					<Button className='cdc-card'>
-						<hr></hr>
-						<h4 className='cdc-click'>CLICK HERE</h4>{' '}
-						<p className='cdc-p'>
-							To find out more from the CDC about COVID-19 vaccinations and when
-							you can get it in your state
-						</p>
-						<hr></hr>
-					</Button>
-				</a>
+			<Row>
+				<Col xs={3} md={2}></Col>
+				<Col xs={12} md={8}>
+					<a
+						target='_blank'
+						href='https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html'>
+						<Button className='cdc-card'>
+							<hr></hr>
+							<h4 className='cdc-click'>CLICK HERE</h4>{' '}
+							<p className='cdc-p'>
+								To find out more from the CDC about COVID-19 vaccinations and
+								when you can get it in your state
+							</p>
+							<hr></hr>
+						</Button>
+					</a>
+				</Col>
+				<Col xs={3} md={2}></Col>
 			</Row>
 			<BadgeModal show={show} handleClose={handleClose} />
-		</div>
+		</Container>
 	);
 };
 
